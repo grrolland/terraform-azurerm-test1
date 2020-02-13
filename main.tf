55f65dc3ff4a9d85d7b7b7493ce68ddc9b92715a
grrolland <gregoire.rolland@live.fr>
2020-02-13 17:09:28 +0100

    Remove ids

--- main.tf
+++ main.tf
@@ -2,7 +2,4 @@
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.44.0"
-
-  subscription_id = "c02e7ca5-0776-48ea-9de0-e68a8875a741"
-  tenant_id       = "10430f61-d7a1-4bff-88cf-a5f03830e099"
 }
