0c770cc51ad05d4d0ef526e98c028b2ff9221458
grrolland <gregoire.rolland@live.fr>
2020-02-13 17:13:18 +0100

    Add ressource group

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.44.0"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "testResourceGroup1"
+  location = "West US"
+
+  tags = {
+    environment = "Production"
+  }
+}
